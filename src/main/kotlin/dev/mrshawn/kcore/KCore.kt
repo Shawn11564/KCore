0b5d9ed615b2a3d1c7cc9e54739636c5462f79f8
Shawn11564 <thunderboy288@gmail.com>
2021-11-08 16:41:31 -0500

    Initial commit

---
+++ src/main/kotlin/dev/mrshawn/kcore/KCore.kt
@@ -0,0 +1,15 @@
+package dev.mrshawn.kcore
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class KCore : JavaPlugin() {
+
+	override fun onEnable() {
+		// Plugin startup logic
+	}
+
+	override fun onDisable() {
+		// Plugin shutdown logic
+	}
+	
+}
